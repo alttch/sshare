2b1e8df910f8053119976b7b1fc18df7bbd0fe25
Sergei S <div@altertech.com>
2020-11-04 04:10:56 +0100

    import

---
+++ Makefile
@@ -0,0 +1,27 @@
+VERSION=0.0.1
+
+all:
+	@echo "Select target"
+
+ver:
+	find . -type f -name "*.py" -exec \
+			sed -i "s/^__version__ = .*/__version__ = '${VERSION}'/g" {} \;
+	find ./bin -type f -exec sed -i "s/^__version__ = .*/__version__ = '${VERSION}'/g" {} \;
+
+clean:
+	rm -rf dist build sshare.egg-info
+
+d: clean sdist
+
+sdist:
+	python3 setup.py sdist
+
+build: clean build-packages
+
+build-packages:
+	python3 setup.py build
+
+pub: d pub-pypi
+
+pub-pypi:
+	twine upload dist/*
---
+++ setup.py
@@ -0,0 +1,28 @@
+__version__ = '0.0.1'
+
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='sshare',
+                 version=__version__,
+                 author='Altertech',
+                 author_email='pr@altertech.com',
+                 description='Secure share client',
+                 long_description=long_description,
+                 long_description_content_type='text/markdown',
+                 url='https://github.com/alttch/sshare',
+                 packages=setuptools.find_packages(),
+                 include_package_data=True,
+                 license='Apache License 2.0',
+                 install_requires=[
+                     'requests', 'pyyaml', 'requests_toolbelt', 'tqdm',
+                     'neotermcolor'
+                 ],
+                 classifiers=(
+                     'Programming Language :: Python :: 3',
+                     'License :: OSI Approved :: Apache Software License',
+                     'Topic :: Communications',
+                 ),
+                 scripts=['bin/sshare'])
